ba9595b496bd2931bc386896e3920486f6ff9f3b
Anyetasso <anye-tasso@hotmail.com>
2022-07-09 10:50:58 -0300

    first commit

---
+++ src/model/PrimeiroPrograma.java
@@ -0,0 +1,20 @@
+package model;
+
+import model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+
+
+
+       /* int a = 5;
+        int b = 3;
+        System.out.println("Hello World! " + (a+b));*/
+
+    }
+}
